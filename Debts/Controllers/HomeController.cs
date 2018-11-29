d46143138e0e71608930497420ba1a6b8a663ac6
Radovan <koncarevicradovan@gmail.com>
2018-11-29 16:59:57 +0100

    Initial commit before EF core

---
+++ Debts/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Debts.Models;
+using Debts.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Debts.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(x=>x.Name);
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to pie shop!",
+                Pies = pies.ToList()
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ Debts/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Debts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Debts.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
