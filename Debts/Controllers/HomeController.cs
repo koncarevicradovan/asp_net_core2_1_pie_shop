855423c9a10e74449544fa87b89209f62a6dce98
Radovan <koncarevicradovan@gmail.com>
2018-11-30 22:05:43 +0100

    Before tag helpers

--- Debts/Controllers/HomeController.cs
+++ Debts/Controllers/HomeController.cs
@@ -27,5 +27,15 @@ namespace Debts.Controllers
             };
             return View(homeViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie==null)
+            {
+                return NotFound();
+            }
+            return View(pie);
+        }
     }
 }
--- Debts/Startup.cs
+++ Debts/Startup.cs
@@ -6,17 +6,28 @@ using Debts.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Debts
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IPieRepository, PieRepository>();
+            //services.AddTransient<IPieRepository, MockPieRepository>();
+
             services.AddMvc();
         }
 
@@ -26,7 +37,12 @@ namespace Debts
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute(); // must be placed after use static files
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
